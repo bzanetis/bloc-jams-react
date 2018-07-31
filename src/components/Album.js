91b0d755e45646e8419bc6619d6506d526e3c8be
bzanetis <britt.zanetis19@gmail.com>
2018-07-31 11:53:48 -0500

    Checkpoint 8 player bar buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 
 
@@ -47,6 +48,14 @@ class Album extends Component {
         }
     }
 
+    handlePrevClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(0, currentIndex - 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
     playPauseNumber(index) {
 
 	      if (this.state.currentSong.title === this.state.album.songs[index].title && this.state.isPlaying === true){
@@ -89,6 +98,12 @@ render() {
             )}
            </tbody>
       </table>
+       <PlayerBar
+        isPlaying={this.state.isPlaying}
+        currentSong={this.state.currentSong}
+        handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+        handlePrevClick={() => this.handlePrevClick()}
+       />
      </section>
      );
    }
